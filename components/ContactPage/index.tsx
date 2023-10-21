571438507123897761d1c5972e9fb1cdade24566
NhoNH <nhoxtvt@gmail.com>
2023-10-22 00:12:25 +0700

    add contact page

---
+++ components/ContactPage/index.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+
+type Props = {};
+
+const ContactPage = (props: Props) => {
+  return (
+    <form className="w-[724px] border-b border-gray-900/10 pb-12">
+      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="first-name"
+            className="block text-sm font-black leading-6 text-gray-900"
+          >
+            First name
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              autoComplete="given-name"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+            />
+          </div>
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="last-name"
+            className="block text-sm font-black leading-6 text-gray-900"
+          >
+            Last name
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              autoComplete="family-name"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+            />
+          </div>
+        </div>
+
+        <div className="col-span-full">
+          <label
+            htmlFor="email"
+            className="block text-sm font-black leading-6 text-gray-900"
+          >
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+            />
+          </div>
+        </div>
+
+        <div className="col-span-full">
+          <label
+            htmlFor="street-address"
+            className="block text-sm font-black leading-6 text-gray-900"
+          >
+            Subject
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="street-address"
+              id="street-address"
+              autoComplete="street-address"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+            />
+          </div>
+        </div>
+
+        <div className="col-span-full">
+          <label
+            htmlFor="about"
+            className="block text-sm font-black leading-6 text-gray-900"
+          >
+            About
+          </label>
+          <div className="mt-2">
+            <textarea
+              id="about"
+              name="about"
+              rows={3}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
+            ></textarea>
+          </div>
+          <p className="mt-3 text-sm leading-6 text-gray-600">
+            Let me know what you want to say here
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center justify-start mt-4">
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Send
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default ContactPage;
---
+++ pages/contact/index.tsx
@@ -0,0 +1,18 @@
+import ContactPage from "@/components/ContactPage";
+import Title from "@/components/Title";
+import React from "react";
+
+type Props = {};
+
+const Contact = (props: Props) => {
+  return (
+    <div className="flex flex-col items-center px-24 py-8 bg-[#E6DACE]">
+      <div className="my-20">
+        <Title label="Let's talk" />
+      </div>
+      <ContactPage />
+    </div>
+  );
+};
+
+export default Contact;
