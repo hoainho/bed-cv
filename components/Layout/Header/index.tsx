5e57cb044f49e4caf2360eca38986ef83ad986cc
NhoNH <nhoxtvt@gmail.com>
2023-10-30 20:28:51 +0700

    add responve for resume page

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -1,12 +1,13 @@
 import Title from "@/components/Title";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SlArrowRight } from "react-icons/sl";
 
 type Props = {};
 
 const Header = (props: Props) => {
+  const refTooltipNav = useRef(null);
   const [navbar] = useState([
     {
       label: "About me",
@@ -26,7 +27,22 @@ const Header = (props: Props) => {
     },
   ]);
   const router = useRouter();
-  const [openNavbar, setOpenNavbar] = useState(false)
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  useEffect(() => {
+    const handleOutSideClick = (event: any) => {
+      if (openNavbar && !(refTooltipNav.current as any)?.contains(event.target)) {
+        console.log("Outside Clicked. ");
+        setOpenNavbar(false);
+      }
+    };
+
+    window.addEventListener("mousedown", handleOutSideClick);
+
+    return () => {
+      window.removeEventListener("mousedown", handleOutSideClick);
+    };
+  }, [refTooltipNav, openNavbar]);
   return (
     <div className="flex items-center justify-between px-24 py-14 relative sm:sticky top-0 z-10 bg-white">
       <div className="flex justify-start items-center flex-col sm:flex-row">
@@ -59,7 +75,7 @@ const Header = (props: Props) => {
             aria-hidden="true"
           >
             <path
-              stroke-linecap="round"
+              strokeLinecap="round"
               stroke-linejoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
@@ -73,14 +89,19 @@ const Header = (props: Props) => {
             aria-hidden="true"
           >
             <path
-              stroke-linecap="round"
+              strokeLinecap="round"
               stroke-linejoin="round"
               d="M6 18L18 6M6 6l12 12"
             />
           </svg>
         </button>
       </div>
-      <div className={`justify-start items-start sm:items-center ${openNavbar ? "navbar-mobile-display" : "navbar-mobile-hidden"} sm:flex navbar-mobile`}>
+      <div
+        className={`justify-start items-start sm:items-center ${
+          openNavbar ? "navbar-mobile-display" : "navbar-mobile-hidden"
+        } sm:flex navbar-mobile`}
+        ref={refTooltipNav}
+      >
         {navbar.map((item, index) => (
           <div key={index} className="navbar-item mx-2">
             <Link
--- components/ResumePage/index.tsx
+++ components/ResumePage/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Title from "../Title";
 import { GiBearFace } from "react-icons/gi";
-import { BiCloudDownload } from 'react-icons/bi'
+import { BiCloudDownload } from "react-icons/bi";
 type Props = {};
 
 const ResumePage = (props: Props) => {
@@ -37,165 +37,141 @@ const ResumePage = (props: Props) => {
       label: "Spanish (proficient)",
     },
   ]);
+  const [projects, setProjects] = useState([
+    {
+      id: 1,
+      timeline: "2019 - 2020",
+      position: "UI Designer",
+      companyName: "Hutech University",
+      location: "Ho Chi Minh, Viet Nam",
+      description: `I'm a paragraph. Click here to add your own text and edit me.
+      It’s easy. Just click “Edit Text” or double click me to add
+      your own content and make changes to the font.`,
+    },
+    {
+      id: 2,
+      timeline: "2020 - 2021",
+      position: "UI/UX Designer",
+      companyName: "Hutech University",
+      location: "Ho Chi Minh, Viet Nam",
+      description: `lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.`,
+    },
+  ]);
+  const [educations, setEducations] = useState([
+    {
+      id: 1,
+      timeline: "2022 - Present",
+      position: "UI Designer",
+      companyName: "Hutech University",
+      location: "Ho Chi Minh, Viet Nam",
+      description: `I'm a paragraph. Click here to add your own text and edit me.
+      It’s easy. Just click “Edit Text” or double click me to add
+      your own content and make changes to the font.`,
+    }
+  ]);
   return (
-    <div className="flex flex-col items-center px-24 py-8 bg-[#E6DACE]">
+    <div className="flex flex-col items-center px-5 sm:px-24 py-8 bg-[#E6DACE]">
       <div className="my-20">
         <Title label="Resume" />
       </div>
-      <div className="w-[724px]">
+      <div className="w-full sm:w-[724px]">
         <div className="w-full mb-12">
           {/* Title */}
           <div className="flex justify-between items-center mb-12">
             <h1
-              className={`font-black text-[24px] my-2 tracking-widest uppercase`}
+              className={`font-black text-[22px] sm:text-[24px] my-2 tracking-widest capitalize sm:uppercase`}
             >
               Experience
             </h1>
             <button
               type="button"
-              className="py-1 px-8 bg-primary text-white uppercase rounded-3xl h-[40px] flex items-center justify-center"
+              className="py-1 px-6 sm:px-8 bg-primary text-white uppercase rounded-3xl h-[34px] sm:h-[40px] flex items-center justify-center text-[14px] sm:text-[24px]"
             >
-              Download cv <BiCloudDownload className="ml-2"/>
+              Download cv <BiCloudDownload className="ml-2" />
             </button>
           </div>
           {/* Content */}
-          <div className="px-12 py-20 bg-white mb-12">
+          {projects.map((project) => (
+            <div className="px-12 py-20 bg-white mb-12" key={project.id}>
             <h1 className="text-primary font-bold text-2xl mb-4">
-              2019 - 2020
+              {project.timeline}
             </h1>
-            <div className="w-full flex justify-between items-start text-lg">
-              <div className="w-50">
+            <div className="w-full flex flex-col sm:flex-row justify-between items-start text-lg">
+              <div className="w-full sm:w-50 mb-3 sm:mb-0">
                 {/* Job position */}
                 <h1 className="font-light uppercase text-medium mb-3">
-                  Designer
+                  {project.position}
                 </h1>
                 {/* Company name */}
-                <h2 className="font-light text-medium mb-3">
-                  Hutech University
+                <h2 className="font-light text-medium mb-1 sm:mb-3">
+                  {project.companyName}
                 </h2>
                 {/* Company location */}
                 <h3 className="font-light text-medium mb-3">
-                  Ho Chi Minh, Viet Nam
+                  {project.location}
                 </h3>
               </div>
-              <div className="w-50">
+              <div className="w-full sm:w-50">
                 <p className="font-light text-medium">
-                  I'm a paragraph. Click here to add your own text and edit me.
-                  It’s easy. Just click “Edit Text” or double click me to add
-                  your own content and make changes to the font.
+                  {project.description}
                 </p>
               </div>
             </div>
           </div>
-          <div className="px-12 py-20 bg-white">
-            <h1 className="text-primary font-bold text-2xl mb-4">
-              2019 - 2020
-            </h1>
-            <div className="w-full flex justify-between items-start text-lg">
-              <div className="w-50">
-                {/* Job position */}
-                <h1 className="font-light uppercase text-medium mb-3">
-                  Designer
-                </h1>
-                {/* Company name */}
-                <h2 className="font-light text-medium mb-3">
-                  Hutech University
-                </h2>
-                {/* Company location */}
-                <h3 className="font-light text-medium mb-3">
-                  Ho Chi Minh, Viet Nam
-                </h3>
-              </div>
-              <div className="w-50">
-                <p className="font-light text-medium">
-                  I'm a paragraph. Click here to add your own text and edit me.
-                  It’s easy. Just click “Edit Text” or double click me to add
-                  your own content and make changes to the font.
-                </p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
 
         <div className="w-full mb-12">
           {/* Title */}
           <div className="flex justify-between items-center mb-12">
             <h1
-              className={`font-black text-[24px] my-2 tracking-widest uppercase`}
+              className={`font-black text-[22px] sm:text-[24px] my-2 tracking-widest capitalize sm:uppercase`}
             >
               Education
             </h1>
           </div>
           {/* Content */}
-          <div className="px-12 py-20 bg-white mb-12">
-            <h1 className="text-primary font-bold text-2xl mb-4">
-              2020 - 2021
-            </h1>
-            <div className="w-full flex justify-between items-start text-lg">
-              <div className="w-50">
-                {/* Job position */}
-                <h1 className="font-light uppercase text-medium mb-3">
-                  Designer
-                </h1>
-                {/* Company name */}
-                <h2 className="font-light text-medium mb-3">
-                  Hutech University
-                </h2>
-                {/* Company location */}
-                <h3 className="font-light text-medium mb-3">
-                  Ho Chi Minh, Viet Nam
-                </h3>
-              </div>
-              <div className="w-50">
-                <p className="font-light text-medium">
-                  I'm a paragraph. Click here to add your own text and edit me.
-                  It’s easy. Just click “Edit Text” or double click me to add
-                  your own content and make changes to the font.
-                </p>
-              </div>
+          {educations.map((education) => (
+          <div className="px-12 py-20 bg-white mb-12" key={education.id}>
+          <h1 className="text-primary font-bold text-2xl mb-4">
+            {education.timeline}
+          </h1>
+          <div className="w-full flex flex-col sm:flex-row justify-between items-start text-lg">
+            <div className="w-full sm:w-50 mb-3 sm:mb-0">
+              {/* Job position */}
+              <h1 className="font-light uppercase text-medium mb-3">
+                {education.position}
+              </h1>
+              {/* Company name */}
+              <h2 className="font-light text-medium mb-1 sm:mb-3">
+                {education.companyName}
+              </h2>
+              {/* Company location */}
+              <h3 className="font-light text-medium mb-3">
+                {education.location}
+              </h3>
             </div>
-          </div>
-          <div className="px-12 py-20 bg-white">
-            <h1 className="text-primary font-bold text-2xl mb-4">
-              2020 - 2022
-            </h1>
-            <div className="w-full flex justify-between items-start text-lg">
-              <div className="w-50">
-                {/* Job position */}
-                <h1 className="font-light uppercase text-medium mb-3">
-                  Designer
-                </h1>
-                {/* Company name */}
-                <h2 className="font-light text-medium mb-3">
-                  Hutech University
-                </h2>
-                {/* Company location */}
-                <h3 className="font-light text-medium mb-3">
-                  Ho Chi Minh, Viet Nam
-                </h3>
-              </div>
-              <div className="w-50">
-                <p className="font-light text-medium">
-                  I'm a paragraph. Click here to add your own text and edit me.
-                  It’s easy. Just click “Edit Text” or double click me to add
-                  your own content and make changes to the font.
-                </p>
-              </div>
+            <div className="w-full sm:w-50">
+              <p className="font-light text-medium">
+                {education.description}
+              </p>
             </div>
           </div>
         </div>
+          ))}
+        </div>
 
         {/* Content */}
-        <div className="w-full px-12 py-20 bg-white mb-12">
-          <div className="my-8">
-            <h1 className={`font-black text-3xl mb-12 tracking-widest`}>
+        <div className="w-full px-12 py-16 sm:py-20 bg-white mb-12">
+          <div className="my-4 sm:my-8">
+            <h1 className={`font-black text-[22px] sm:text-3xl mb-12 tracking-widest`}>
               Professional skillset
             </h1>
-            <div className="w-full flex flex-wrap justify-between items-start text-lg">
+            <div className="w-full flex flex-col sm:flex-row flex-nowrap sm:flex-wrap justify-between items-start text-lg">
               {skills.map((skill) => (
                 <div
                   key={skill.id}
-                  className="w-50 flex justify-start items-center mb-5"
+                  className="w-full sm:w-50 flex justify-start items-center mb-5"
                 >
                   <GiBearFace size={15} color="#0D6EFD" className="mr-2" />
                   <h1 className="font-light text-medium">{skill.label}</h1>
@@ -203,15 +179,15 @@ const ResumePage = (props: Props) => {
               ))}
             </div>
           </div>
-          <div className="my-8">
-            <h1 className={`font-black text-3xl mb-12 tracking-widest`}>
+          <div className="my-4 sm:my-8">
+            <h1 className={`font-black text-[22px] sm:text-3xl mb-12 tracking-widest`}>
               Languages
             </h1>
-            <div className="w-full flex flex-wrap justify-between items-start text-lg">
+            <div className="w-full flex flex-col sm:flex-row flex-nowrap sm:flex-wrap justify-between items-start text-lg">
               {languages.map((lang) => (
                 <div
                   key={lang.id}
-                  className="w-50 flex justify-start items-center mb-5"
+                  className="w-full sm:w-50 flex justify-start items-center mb-5"
                 >
                   <GiBearFace size={15} color="#0D6EFD" className="mr-2" />
                   <h1 className="font-light text-medium">{lang.label}</h1>
