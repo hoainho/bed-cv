37cc97712f16853140a23582c0bc916592eb0542
NhoNH <nhoxtvt@gmail.com>
2023-11-12 20:52:09 +0700

    update content for project page

--- components/ProjectPage/index.tsx
+++ components/ProjectPage/index.tsx
@@ -7,15 +7,22 @@ const ProjectPage = () => {
       id: 1,
       name: "Mushroom Design",
       role: "Graphic Designer",
-      description: `Use the Gaussian Blur tool to blur the background, the Object Selection Tool to crop the mushroom image, then go to Hue/ Saturation, adjust the appropriate color and select the Color Dodge filter, select the newly adjusted color layer, press (Ctrl + i) to turn the layer black, and use the Brush Tool to reduce the Opacity.`,
+      description: `Use the Gaussian Blur tool to blur the blackground, then use the Object Selection Tool to cut the mushroom image, then go to Levels to reduce the image's brightness, then select the mushroom layer and go to Hue/Saturation to adjust the color. Finally, use the Brush Tool to paint highlights.`,
       image: "/projects/project1.jpg",
     },
     {
       id: 2,
       name: "Cosmetic Design",
       role: "Graphic Designer",
-      description: `The Gradient Tool is used to assist adjust the color of the backdrop, and the pen tool is used to draw the base of the box (yellow). To create a shadow effect for the frame, go to the frame drawing window and select Drop Shadow. Select the frame layer, then hold down the Alt key and drag the cloud picture beneath the frame layer. Crop the image using the Object Selection Tool, add a shadow effect on the product with the Drop Shadow tool, then add text using the Horizontal Type Tool.`,
+      description: `The Gradient Tool is used to help modify the color of the backdrop, the pen tool is used to form the base of the box, add the cloud picture to the frame drawing window, crop the image using the Object Selection Tool, and produce the effect. The Drop Shadow tool was used to create a shadow for the object, and the Horizontal Type Tool was used to create text.`,
       image: "/projects/project2.jpg",
+    },
+    {
+      id: 3,
+      name: "Cosmetic Design",
+      role: "Graphic Designer",
+      description: `Change the color with the Gradient Tool, then blur with Gaussian Blur, crop the picture with the Object Selection Tool, then cut the can into three sections with the Pen Tool.`,
+      image: "/projects/project3.jpg",
     },
   ]);
   return (
@@ -32,7 +39,7 @@ const ProjectPage = () => {
               </h1>
               <p className="font-bold text-lg mb-8">{project.role}</p>
             </div>
-            <p className="font-light text-xl">{project.description}</p>
+            <p className="font-light text-xl text-justify break-word">{project.description}</p>
           </div>
           <div className="w-full sm:w-5/12 relative h-full">
             <img
